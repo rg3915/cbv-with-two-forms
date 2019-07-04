26483462523bfc4e208d89b5d5f1d301d25fd16d
rg3915 <rg3915@yahoo.com.br>
2019-07-04 15:33:35 -0300

    Adicionando forms.

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
 from .models import Book, Movie
+from .forms import BookForm
 
 
 class Index(TemplateView):
     template_name = 'index.html'
+
+
+class BookCreate(CreateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'form.html'
